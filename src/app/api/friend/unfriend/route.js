d923e1657a442ebfd5aa360bffbebf31ad26dcad
Amrit Niure <niureamrit2@gmail.com>
2023-10-14 23:22:28 +1100

    after deleting the message trigger a event that will remove the friend in the sidebar section in realtime. 11:22PM  10/14/2023

--- src/app/api/friend/unfriend/route.js
+++ src/app/api/friend/unfriend/route.js
@@ -1,10 +1,12 @@
 import { authOptions } from "@/lib/auth"
 import connectionDB from "@/lib/db"
+import Chat from "@/modal/chatSchema"
+import Message from "@/modal/messageSchema"
 import User from "@/modal/userSchema"
 import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server"
 
-export async function POST(req){
+export async function POST(req) {
     const { id } = await req.json()
     if (!id) {
         return new Response("Please provide an id", { status: 400 })
@@ -28,28 +30,65 @@ export async function POST(req){
         return NextResponse.json({ status: 'success', message: `Your both are not friends` }, { status: 401 });
     }
 
-// delete message between two users when unfriended and perform realtime functionality in sidebar to remove the friends by removing the friend form the friends state variable 
+    // delete message between two users when unfriended and perform realtime functionality in sidebar to remove the friends by removing the friend form the friends state variable 
 
     try {
         // delete the friends of both user
         const updatedUser1 = await User.findByIdAndUpdate(
-           session_user._id,
-           { $pull: { friends: friend._id } },
-           { new: true }
-       );
-       if (!updatedUser1) {
-           return NextResponse.json({ message: "User Not found !" }, { status: 404 });
-       }
+            session_user._id,
+            { $pull: { friends: friend._id } },
+            { new: true }
+        );
+        if (!updatedUser1) {
+            return NextResponse.json({ message: "User Not found !" }, { status: 404 });
+        }
         const updatedUser2 = await User.findByIdAndUpdate(
-           friend._id,
-           { $pull: { friends: session_user._id } },
-           { new: true }
-       );
-       if (!updatedUser2) {
-           return NextResponse.json({ message: "User Not found !" }, { status: 404 });
-       }
-   } catch (error) {
-       console.log("Error while making changes related to unfriend in database")
-   }
-   return NextResponse.json({ success : true , message: "Unfriend Task Completed" }, { status: 200 });
+            friend._id,
+            { $pull: { friends: session_user._id } },
+            { new: true }
+        );
+        if (!updatedUser2) {
+            return NextResponse.json({ message: "User Not found !" }, { status: 404 });
+        }
+       
+        // Step 1: Find chat and get message IDs
+        const chat = await Chat.findOne({
+            participants: { $all: [session_user._id, friend._id] },
+        });
+
+        if (!chat) {
+            console.log('Chat not found');
+            return; // or handle accordingly
+        }
+
+        const messageIds = chat.messages;
+
+        // Step 2: Delete messages
+        const deletedMessages = await Message.deleteMany({
+            _id: { $in: messageIds },
+        });
+        const deletedChat = await Chat.deleteOne({
+            participants: { $all: [session_user._id, friend._id] },
+        });
+
+        // Check if the chat and messages were deleted successfully
+        if (deletedChat.deletedCount === 1) {
+            console.log('Chat deleted successfully');
+        } else {
+            console.log('Chat deletion failed');
+        }
+
+        if (deletedMessages.deletedCount > 0) {
+            console.log(`${deletedMessages.deletedCount} messages deleted successfully`);
+        } else {
+            console.log('No messages deleted');
+        }
+
+
+
+        // after deleting the message trigger a event that will remove the friend in the sidebar section in realtime.
+    } catch (error) {
+        console.log("Error while making changes related to unfriend in database")
+    }
+    return NextResponse.json({ success: true, message: "Unfriend Task Completed" }, { status: 200 });
 }
